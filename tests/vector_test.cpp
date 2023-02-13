e86b99cc7d1873c9aeb66535563b52b6463e45d1
severin-denisenko <devchonkavkorobchonke@gmail.com>
2023-02-14 00:47:34 +0300

    Cover vector with tests

--- tests/vector_test.cpp
+++ tests/vector_test.cpp
@@ -3,31 +3,59 @@
 //
 
 #include "SLib/SVector.hpp"
-#include "SLib/SLog.hpp"
+#include "SLib/SLib.hpp"
 
-int main(){
-    S_LOG_LEVEL_INFO;
+S_TEST(Vector, Creation){
+    slib::SVector<int> vector;
+    S_EXPECT_EQ(vector.size(), 0u);
+    vector.push_back(1);
+    S_EXPECT_EQ(vector.size(), 1u);
+    S_EXPECT_EQ(vector[0], 1);
+    vector.pop_back();
+    S_EXPECT_EQ(vector.size(), 0u);
+}
 
+S_TEST(Vector, Resizing){
+    slib::SVector<int> vector;
+    vector.push_back(1);
+    vector.push_back(1);
+    vector.push_back(1);
+    vector.push_back(1);
+    vector.push_back(1);
+    vector.push_back(1);
+    vector.push_back(1);
+    vector.push_back(1);
+    S_EXPECT_EQ(vector.size(), 8u);
+    vector.resize(2);
+    S_EXPECT_EQ(vector.size(), 2u);
+    vector.shrink_to_fit();
+    S_EXPECT_EQ(vector.capacity(), 2u);
+    vector.resize(100);
+    S_EXPECT_EQ(vector.capacity(), 100u);
+    vector.clear();
+    S_EXPECT_EQ(vector.size(), 0u);
+}
+
+S_TEST(Vector, Iterator){
     slib::SVector<int> vector;
 
-    for (int i = 0; i < 100; ++i) {
-        vector.push_back(i);
+    for (int i = 1; i <= 100; ++i) {
+        vector.emplace_back(i);
     }
 
-    slib::SVector<int> vector1(vector);
-    slib::SVector<int> vector2(vector);
-
-    for (int i = 0; i < 100; ++i) {
-        vector1.emplace_back(i + 100);
+    int sum = 0;
+    for (auto item : vector) {
+        sum += item;
     }
 
-    vector1.resize(1000);
+    S_EXPECT_EQ(sum, 5050);
+}
 
-    for (auto &item: vector1) {
-        S_INFO(item);
-    }
+int main(){
+    S_REGISTER_TEST(Vector, Creation);
+    S_REGISTER_TEST(Vector, Resizing);
+    S_REGISTER_TEST(Vector, Iterator);
 
-    vector1.shrink_to_fit();
-
+    S_RUN_TESTS();
     return 0;
 }
